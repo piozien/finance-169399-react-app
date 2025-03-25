2524d766a944dbff132cf7ee129a050a06075236
Piotr Zienowicz <115182798+piozien@users.noreply.github.com>
2025-03-25 22:17:01 +0100

    Update axios.js

--- fe-finance/src/api/axios.js
+++ fe-finance/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://finance-dashboard-etg7.onrender.com:8080';
+const API_URL = process.env.REACT_APP_API_URL;
 const axiosInstance = axios.create({
     baseURL: API_URL,
     headers: {
