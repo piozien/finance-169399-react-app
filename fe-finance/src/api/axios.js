f9e930148c6e5a00c3f19aee0bb577a2ad910d6f
Piotr Zienowicz <115182798+piozien@users.noreply.github.com>
2025-03-26 17:44:33 +0100

    icon change

--- fe-finance/src/api/axios.js
+++ fe-finance/src/api/axios.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
 const API_URL = import.meta.env.VITE_API_URL || 'https://finance-169399.herokuapp.com/api';
-console.log("API request to:", `${API_URL}/auth/register`);
 
 const axiosInstance = axios.create({
     baseURL: API_URL,
