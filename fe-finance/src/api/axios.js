86b859fad09ec457efd23122e9253afdc67e079b
Piotr Zienowicz <115182798+piozien@users.noreply.github.com>
2025-03-26 08:09:12 +0100

    axios update

--- fe-finance/src/api/axios.js
+++ fe-finance/src/api/axios.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL;
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:8080';
+
 const axiosInstance = axios.create({
     baseURL: API_URL,
     headers: {
