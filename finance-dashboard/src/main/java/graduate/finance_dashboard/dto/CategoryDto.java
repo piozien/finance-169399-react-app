22077f752228639c5823ae8a5b2fc8b6d3f6e36e
Piotr Zienowicz <115182798+piozien@users.noreply.github.com>
2025-03-27 17:28:38 +0100

    backend moved to another repository

--- finance-dashboard/src/main/java/graduate/finance_dashboard/dto/CategoryDto.java
+++
@@ -1,9 +0,0 @@
-package graduate.finance_dashboard.dto;
-
-import lombok.Data;
-
-@Data
-public class CategoryDto {
-    private Long id;
-    private String name;
-}
--- finance-dashboard/src/main/java/graduate/finance_dashboard/dto/LoginRequest.java
+++
@@ -1,9 +0,0 @@
-package graduate.finance_dashboard.dto;
-
-import lombok.Data;
-
-@Data
-public class LoginRequest {
-    private String email;
-    private String password;
-}
